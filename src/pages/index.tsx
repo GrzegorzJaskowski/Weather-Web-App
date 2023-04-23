ce1389bdb811125d1d436f2f7c191c84c388dc80
GrzegorzJaskowski <grzegorz.jaskowski81@gmail.com>
2023-04-23 23:30:31 +0200

    use dayjs in date and time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,36 @@
 import { type NextPage } from "next";
 import { WeatherItem } from "~/components/weatherItem";
+import dayjs from "dayjs";
 
-function getTime() {
-  const now = new Date();
-  return (
-    now.getHours().toString().padStart(2, "0") +
-    ":" +
-    now.getMinutes().toString().padStart(2, "0")
-  );
-}
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const day = days[dayjs().day()]?.toString();
 
-function getDate() {
-  const now = new Date();
-  return (
-    now.toLocaleDateString("en-EN", { weekday: "long" }) +
-    " " +
-    now.getDate().toString().padStart(2, "0") +
-    "." +
-    now.getMonth().toString().padStart(2, "0")
-  );
-}
+const time =
+  dayjs().hour().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
+  ":" +
+  dayjs().minute().toLocaleString("en-US", { minimumIntegerDigits: 2 });
+
+const date =
+  (day ? day : "") +
+  " " +
+  dayjs().date().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
+  "." +
+  dayjs().month().toLocaleString("en-US", { minimumIntegerDigits: 2 });
 
 const Home: NextPage = () => {
   return (
     <main className="flex h-screen">
       <div className="relative flex h-full w-full bg-gradient-to-t from-slate-800 to-cyan-800">
         <div className="absolute flex h-1/4 w-full place-items-center justify-center">
-          <div className="text-4xl text-slate-400">{getTime()}</div>
+          <div className="text-4xl text-slate-400">{time}</div>
         </div>
         <div className="absolute flex h-full w-full items-center justify-center">
           <div className="grid grid-cols-2 grid-rows-2 place-items-center gap-12">
@@ -37,7 +41,7 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className="absolute bottom-0 flex h-1/4 w-full place-items-center justify-center">
-          <div className="text-4xl text-slate-400">{getDate()}</div>
+          <div className="text-4xl text-slate-400">{date}</div>
         </div>
       </div>
     </main>
