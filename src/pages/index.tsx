f7b05f2d3768ee316526a0a8f79aadfd18c3b338
GrzegorzJaskowski <grzegorz.jaskowski81@gmail.com>
2023-04-24 23:10:05 +0200

    fixed react hydration error with time and date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,24 +13,29 @@ const days = [
 ];
 const day = days[dayjs().day()]?.toString();
 
-const time =
-  dayjs().hour().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
-  ":" +
-  dayjs().minute().toLocaleString("en-US", { minimumIntegerDigits: 2 });
-
-const date =
-  (day ? day : "") +
-  " " +
-  dayjs().date().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
-  "." +
-  dayjs().month().toLocaleString("en-US", { minimumIntegerDigits: 2 });
+function getTime() {
+  return (
+    dayjs().hour().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
+    ":" +
+    dayjs().minute().toLocaleString("en-US", { minimumIntegerDigits: 2 })
+  );
+}
+function getDate() {
+  return (
+    (day ? day : "") +
+    " " +
+    dayjs().date().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
+    "." +
+    dayjs().month().toLocaleString("en-US", { minimumIntegerDigits: 2 })
+  );
+}
 
 const Home: NextPage = () => {
   return (
     <main className="flex h-screen">
-      <div className="relative flex h-full w-full bg-gradient-to-t from-slate-800 to-cyan-800">
+      <div className="from-slate-800 to-cyan-800 relative flex h-full w-full bg-gradient-to-t">
         <div className="absolute flex h-1/4 w-full place-items-center justify-center">
-          <div className="text-4xl text-slate-400">{time}</div>
+          <div className="text-slate-400 text-4xl">{getTime()}</div>
         </div>
         <div className="absolute flex h-full w-full items-center justify-center">
           <div className="grid grid-cols-2 grid-rows-2 place-items-center gap-12">
@@ -41,7 +46,7 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className="absolute bottom-0 flex h-1/4 w-full place-items-center justify-center">
-          <div className="text-4xl text-slate-400">{date}</div>
+          <div className="text-slate-400 text-4xl">{getDate()}</div>
         </div>
       </div>
     </main>
