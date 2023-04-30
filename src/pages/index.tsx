2d907818b3d9c62be59b9ea5311761d5729d7d0e
GrzegorzJaskowski <grzegorz.jaskowski81@gmail.com>
2023-04-30 21:31:30 +0200

    auto refresh date and time with useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,60 +5,61 @@ import { WiThermometer } from "react-icons/wi";
 import { WiBarometer } from "react-icons/wi";
 import { WiDaySunny } from "react-icons/wi";
 import { WiSmog } from "react-icons/wi";
-WiBarometer;
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const day = days[dayjs().day()]?.toString();
-
-function getTime() {
-  return (
-    dayjs().hour().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
-    ":" +
-    dayjs().minute().toLocaleString("en-US", { minimumIntegerDigits: 2 })
-  ).replace("0", "O");
-}
-function getDate() {
-  return (
-    (day ? day : "") +
-    " " +
-    dayjs().date().toLocaleString("en-US", { minimumIntegerDigits: 2 }) +
-    "." +
-    (dayjs().month() + 1).toLocaleString("en-US", { minimumIntegerDigits: 2 })
-  ).replace("0", "O");
-}
+import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
-  // const [lat, setLat] = useState(0);
-  // const [long, setLong] = useState(0);
-  // const [data, setData] = useState();
+  const [time, setTime] = useState(
+    (
+      dayjs().hour(dayjs().hour()).format("HH") +
+      ":" +
+      dayjs().minute(dayjs().minute()).format("mm")
+    ).replaceAll("0", "O")
+  );
+  const [date, setDate] = useState(
+    (
+      dayjs().day(dayjs().day()).format("dddd") +
+      " " +
+      dayjs().date(dayjs().date()).format("DD") +
+      "." +
+      dayjs().month(dayjs().month()).format("MM")
+    ).replaceAll("0", "O")
+  );
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     navigator.geolocation.getCurrentPosition((position) => {
-  //       setLat(position.coords.latitude);
-  //       setLong(position.coords.longitude);
-  //     });
+  useEffect(() => {
+    const timeTimer = setInterval(() => {
+      setTime(
+        (
+          dayjs().hour(dayjs().hour()).format("HH") +
+          ":" +
+          dayjs().minute(dayjs().minute()).format("mm")
+        ).replaceAll("0", "O")
+      );
+    }, (dayjs().second() === 0 ? 60 : 60 - dayjs().second()) * 1000);
 
-  //     const response = await fetch(
-  //       `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=${process.env.REACT_APP_API_KEY}`
-  //     );
-  //     setData(response)
-  //   };
-  // });
+    const dateTimer = setInterval(() => {
+      setDate(
+        (
+          dayjs().day(dayjs().day()).format("dddd") +
+          " " +
+          dayjs().date(dayjs().date()).format("DD") +
+          "." +
+          dayjs().month(dayjs().month()).format("MM")
+        ).replaceAll("0", "O")
+      );
+    }, (dayjs().hour() === 0 ? 23 : 23 - dayjs().hour()) * (dayjs().minute() === 0 ? 59 : 59 - dayjs().minute()) * (dayjs().second() === 0 ? 60 : 60 - dayjs().second()) * 1000);
+
+    return () => {
+      clearInterval(timeTimer);
+      clearInterval(dateTimer);
+    };
+  }, [time, date]);
 
   return (
     <main className="flex h-screen">
       <div className="relative flex h-full w-full bg-gradient-to-t from-slate-800 to-cyan-800">
         <div className="absolute flex h-1/4 w-full place-items-center justify-center">
           <div className="font-dosis text-7xl tracking-wide text-violet-400 subpixel-antialiased">
-            {getTime()}
+            {time}
           </div>
         </div>
         <div className="absolute flex h-full w-full items-center justify-center">
@@ -79,7 +80,7 @@ const Home: NextPage = () => {
         </div>
         <div className="absolute bottom-0 flex h-1/4 w-full place-items-center justify-center">
           <div className="font-dosis text-7xl tracking-wide text-violet-400 subpixel-antialiased">
-            {getDate()}
+            {date}
           </div>
         </div>
       </div>
